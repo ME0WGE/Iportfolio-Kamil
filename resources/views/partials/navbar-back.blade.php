6539b2d4176cda2994916bf23f16c107c6dc8a75
Kamil Baldyga <baldygakamil@gmail.com>
2025-08-22 13:50:50 +0200

    add fontawesome icons to navbar

--- resources/views/partials/navbar-back.blade.php
+++ resources/views/partials/navbar-back.blade.php
@@ -1,17 +1,21 @@
+<head>
+    <script src="https://kit.fontawesome.com/ac1c6017f4.js" crossorigin="anonymous"></script>
+</head>
 <nav>
-    <h1>Back-End navbar</h1>
-    <ul>
-        <li>Log out</li>
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Skill</li>
-        <li>Service</li>
-        <li>Testimonials</li>
-        <li>Contact</li>
-        <li>Mailbox</li>
-    </ul>
-
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li><a href="#contact"><i class="fa-solid fa-right-to-bracket"></i>Log out</a></li>
+        <li><a href="#hero" class="active"><i class="fa-solid fa-home"></i>Home</a></li>
+        <li><a href="#about"><i class="fa-solid fa-user"></i>About</a></li>
+        <li><a href="#portfolio"><i class="fa-solid fa-book"></i>Portfolio</a></li>
+        <li><a href="#portfolio"><i class="fa-solid fa-check-to-slot"></i>Skill</a></li>
+        <li><a href="#services"><i class="fa-solid fa-server"></i>Service</a></li>
+        <li><a href="#services"><i class="fa-solid fa-message"></i>Testimonials</a></li>
+        <li><a href="#contact"><i class="fa-solid fa-phone"></i>Contact</a></li>
+        <li><a href="#contact"><i class="fa-solid fa-envelope"></i>Mailbox</a></li>
+      </ul>
+    </nav>
+    {{-- Copyright --}}
     <span>copyright</span>
     <span>Kamil Baldyga</span>
 </nav>
--- resources/views/partials/navbar-front.blade.php
+++ resources/views/partials/navbar-front.blade.php
@@ -1,14 +1,18 @@
+<head>
+    <script src="https://kit.fontawesome.com/ac1c6017f4.js" crossorigin="anonymous"></script>
+</head>
 <nav>
-    <h1>Front-End navbar</h1>
-    <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Portfolio</li>
-        <li>Services</li>
-        <li>Contact</li>
-        <li>Log In</li>
-    </ul>
-
+    <nav id="navmenu" class="navmenu">
+      <ul>
+        <li><a href="#hero" class="active"><i class="fa-solid fa-home"></i>Home</a></li>
+        <li><a href="#about"><i class="fa-solid fa-user"></i>About</a></li>
+        <li><a href="#portfolio"><i class="fa-solid fa-book"></i>Portfolio</a></li>
+        <li><a href="#services"><i class="fa-solid fa-server"></i>Services</a></li>
+        <li><a href="#contact"><i class="fa-solid fa-envelope"></i>Contact</a></li>
+        <li><a href="#contact"><i class="fa-solid fa-right-to-bracket"></i>Log In</a></li>
+      </ul>
+    </nav>
+    {{-- Copyright --}}
     <span>copyright</span>
     <span>Kamil Baldyga</span>
 </nav>
