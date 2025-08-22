c2309ec09af7adeda7cb48010e232b983df9bd23
Kamil Baldyga <baldygakamil@gmail.com>
2025-08-22 12:26:30 +0200

    add /back to dashboard

---
+++ resources/views/back-end/dashboard.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>iPortfolio</title>
+</head>
+<body>
+    @extends('layouts.back')
+
+    @section('content')
+    <h1>Homepage</h1>
+    @endsection
+    
+</body>
+</html>
