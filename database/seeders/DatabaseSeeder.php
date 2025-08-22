42a5e2a61fd4ca488600a782f6718926837280fa
Kamil Baldyga <baldygakamil@gmail.com>
2025-08-22 10:00:27 +0200

    update DBSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(AboutSeeder::class);
+        $this->call(AvatarSeeder::class);
+        $this->call(SkillSeeder::class);
+        $this->call(PortfolioSeeder::class);
+        $this->call(ServiceSeeder::class);
+        $this->call(TestimonialSeeder::class);
+        $this->call(ContactSeeder::class);
+        $this->call(MessageSeeder::class);
     }
 }
